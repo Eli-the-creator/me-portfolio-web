32fdf99cbc174faf5d3fac59695d31817f8f3617
ELI-THE_CREATOR <neverlookbackeli@gmail.com>
2024-02-23 15:19:38 +0000

    Safari hight fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,26 +4,24 @@ import { LayoutGridDemo } from "./_components/show-time";
 
 export default function Home() {
   return (
-    <main>
-      <main className="relative w-screen h-screen">
-        <div className="fixed w-full h-[calc(100dvh)] z-10">
-          <Image
-            priority={true}
-            fill
-            className="w-full h-full"
-            src="/asset_bg.png"
-            alt="bg"
-          />
-        </div>
+    <main className="relative w-screen h-[calc(100dvh)]">
+      <div className="fixed w-full h-full z-10">
+        <Image
+          priority={true}
+          fill
+          className="w-full h-full"
+          src="/asset_bg.png"
+          alt="bg"
+        />
+      </div>
 
-        <div className="relative  md:px-24 z-50">
-          <NameSection />
-          <p className="text-center text-slate-50">Click to see more</p>
-          <div className="h-[40vh]">
-            <LayoutGridDemo />
-          </div>
+      <div className="relative  md:px-24 z-50">
+        <NameSection />
+        <p className="text-center text-slate-50">Click to see more</p>
+        <div className="h-[40vh]">
+          <LayoutGridDemo />
         </div>
-      </main>
+      </div>
     </main>
   );
 }
